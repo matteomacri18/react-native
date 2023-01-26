3b590fa24f06b8623231564b6b0aa7856a397b6d
Matteo Macrì <matteomacri18@gmail.com>
2023-01-26 21:58:19 +0100

    init

--- base/App.js
+++ base/App.js
@@ -7,40 +7,8 @@ function number() {
 }
 
 export default function App() {
-  const [face, setFace] = useState("");
-  const [img, setImage] = useState(require("./assets/1.png"));
-
-  const rollDice = () => {
-    let n = number();
-    setFace(n);
-    console.log(n);
-    switch (n) {
-      case 1:
-        setImage(require("./assets/1.png"));
-        break;
-      case 2:
-        setImage(require("./assets/2.png"));
-        break;
-      case 3:
-        setImage(require("./assets/3.png"));
-        break;
-      case 4:
-        setImage(require("./assets/4.png"));
-        break;
-      case 5:
-        setImage(require("./assets/5.png"));
-        break;
-      case 6:
-        setImage(require("./assets/6.png"));
-        break;
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{face}</Text>
-      <Image style={styles.img} source={img} />
-      <Button style={styles.button} title="Roll" onPress={rollDice} />
     </View>
   );
 }
