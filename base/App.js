aa858ce81e0da7b93da6dc135cbe98cee3f9c87d
Matteo Macrì <matteomacri18@gmail.com>
2023-01-26 21:21:15 +0100

    dice

--- base/App.js
+++ base/App.js
@@ -1,53 +1,46 @@
-// IMPORT Flatlist = RecyclerView
-import {
-  Flatlist,
-  Button,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Image, Button, FlatList, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-
-// HOOK => mutableStateOf()
-// IMPORT useState
 import { useState } from "react";
+
+function number() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
 export default function App() {
-  // Array of objs with state
-  const [Items, setItems] = useState([
-    { key: 1, item: "Item 1" },
-    { key: 2, item: "Item 2" },
-    { key: 3, item: "Item 3" },
-  ]);
+  const [face, setFace] = useState("");
+  const [img, setImage] = useState(require("./assets/1.png"));
 
-  // Array of objs no state
-  const animals = [
-    { id: 1, name: "Shark" },
-    { id: 2, name: "Lampo" },
-    { id: 3, name: "Dog" },
-    { id: 4, name: "Lmao" },
-    { id: 5, name: "Shark" },
-    { id: 6, name: "Lampo" },
-    { id: 7, name: "Dog" },
-    { id: 8, name: "Lmao" },
-    { id: 9, name: "Shark" },
-    { id: 10, name: "Lampo" },
-    { id: 11, name: "Dog" },
-    { id: 12, name: "Lmao" },
-  ];
-
-  const nothing = () => {};
-
-  // lambda, input animal output Text animal's name
-  const animal = ({ item }) => (
-    <Text style={styles.text} onPress={nothing}>
-      {item.name}
-    </Text>
-  );
+  const rollDice = () => {
+    let n = number();
+    setFace(n);
+    console.log(n);
+    switch (n) {
+      case 1:
+        setImage(require("./assets/1.png"));
+        break;
+      case 2:
+        setImage(require("./assets/2.png"));
+        break;
+      case 3:
+        setImage(require("./assets/3.png"));
+        break;
+      case 4:
+        setImage(require("./assets/4.png"));
+        break;
+      case 5:
+        setImage(require("./assets/5.png"));
+        break;
+      case 6:
+        setImage(require("./assets/6.png"));
+        break;
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <FlatList horizontal data={animals} renderItem={animal}/>
+      <Text style={styles.text}>{face}</Text>
+      <Image style={styles.img} source={img} />
+      <Button style={styles.button} title="Roll" onPress={rollDice} />
     </View>
   );
 }
@@ -55,19 +48,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#854D27",
-  },
-  item: {
-    backgroundColor: "#4ae1fa",
+    backgroundColor: "#ffffff",
+    alignItems: "center",
     justifyContent: "center",
   },
   text: {
-    color: "#ffffff",
-    fontSize: 20,
-    fontStyle: "italic",
-    margin: 40,
-  },
-  btn: {
+    color: "#000000",
+    fontSize: 25,
     margin: 50,
+  },
+  img: {
+    width: "50%",
+    height: "25%",
+    marginBottom: 40,
   },
 });
