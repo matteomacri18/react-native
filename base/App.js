b1952201261df33634d3dbfd1efccb6b60ea7136
Matteo Macrì <matteomacri18@gmail.com>
2023-01-27 20:14:46 +0100

    textinput

--- base/App.js
+++ base/App.js
@@ -1,14 +1,33 @@
-import { Image, Button, FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 
-function number() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
 export default function App() {
+  const [name, setName] = useState("");
+
+  const changeText = (text) => {
+    setName(text);
+  };
+
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>Please write your name:</Text>
+      <TextInput
+        secureTextEntry
+        maxLength={5}
+        style={styles.input}
+        placeholder="e.g John"
+        onChangeText={changeText}
+      />
+      <Text style={styles.text}>Your name is: {name}</Text>
     </View>
   );
 }
@@ -18,16 +37,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ffffff",
     alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
   },
   text: {
     color: "#000000",
-    fontSize: 25,
-    margin: 50,
+    fontSize: 20,
+    margin: 10,
   },
-  img: {
-    width: "50%",
-    height: "25%",
-    marginBottom: 40,
+  input: {
+    textAlign: "center",
+    fontSize: 20,
+    width: 200,
+    borderWidth: 1,
+    borderColor: "#555",
   },
 });
