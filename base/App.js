4467fd6d9e8101dd2a0f132fb7103861f676e6e6
Matteo Macrì <matteomacri18@gmail.com>
2023-01-27 20:56:38 +0100

    init

--- base/App.js
+++ base/App.js
@@ -13,64 +13,7 @@ import { useState } from "react";
 import MashButton from "./CustomComponent";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const changeText = (text) => {
-    setName(text);
-  };
-
-  const onPressHandler = () => {
-    if (name.length > 3) {
-      setSubmitted(!submitted);
-    } else {
-      Alert.alert(
-        "Warning", // title
-        "Name must be longer than 3 chars", // mex
-        [
-          // max three buttons
-          {
-            text: "Ok",
-            onPress: () => console.warn("Ok Pressed!"),
-            style: "destructive",
-          },
-          {
-            text: "Cancel",
-            onPress: () => console.warn("Cancel Pressed!"),
-            style: "destructive",
-          },
-          {
-            text: "Do not show again",
-            onPress: () => console.warn("DNSA Pressed!"),
-            style: "destructive",
-          },
-        ],
-        { cancelable: true } // option for the dialog
-      );
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Please write your name:</Text>
-      <TextInput
-        secureTextEntry
-        maxLength={5}
-        style={styles.input}
-        placeholder="e.g John"
-        onChangeText={changeText}
-      />
-      {/* <Button title={submitted ? "Clear" : "Submit"} onPress={onPressHandler} />
-      { <TouchableOpacity>}
-        inside we have elements we want to make clickable like button
-      </TouchableOpacity> */}
-
-      {/* component */}
-      <MashButton onPressHandler={onPressHandler} title={name} />
-
-      {submitted ? <Text style={styles.text}>Your name is: {name}</Text> : null}
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
@@ -79,17 +22,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     alignItems: "center",
     padding: 20,
-  },
-  text: {
-    color: "#000000",
-    fontSize: 20,
-    margin: 10,
-  },
-  input: {
-    textAlign: "center",
-    fontSize: 20,
-    width: 200,
-    borderWidth: 1,
-    borderColor: "#555",
   },
 });
